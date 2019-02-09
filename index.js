9ce5f458c002f28ef73e505c5ec6931020695d65
jackding96 <jackding96@outlook.com>
2019-02-09 12:32:20 -0500

    center lines logic

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const output = document.getElementById('output');
 
 input.addEventListener('keyup', inputHandler);
 
-function inputHandler(e) {
-  output.value = input.value;
+function inputHandler() {
+  const lines = input.value.split('\n');
+  const signWidth = Math.max(...lines.map(l=>l.length));
+  
+  const centeredLines = lines.map(line => {
+    const leftSpacing = " ".repeat((signWidth - line.length)/2);
+    const rightSpacing = " ".repeat(signWidth - line.length - leftSpacing.length);
+    return `${leftSpacing}${line}${rightSpacing}`;
+  });
+  output.value = centeredLines.join('\n');
 }
