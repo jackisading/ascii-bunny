1860ad3e60428bc73359ff0bf00eae929dbab5c5
jackding96 <jackding96@outlook.com>
2019-02-09 11:40:49 -0500

    bind events

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function testing() {
-  console.log('hello!');
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+input.addEventListener('keyup', inputHandler);
+
+function inputHandler(e) {
+  output.value = input.value;
 }
