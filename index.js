ab11ba664785106801f2899e7af4eaa533006165
jackding96 <jackding96@outlook.com>
2019-02-09 12:38:17 -0500

    sign outline

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const output = document.getElementById('output');
 input.addEventListener('keyup', inputHandler);
 
 function inputHandler() {
+  padding = 3;
   const lines = input.value.split('\n');
-  const signWidth = Math.max(...lines.map(l=>l.length));
+  const signWidth = Math.max(...lines.map(l=>l.length))+padding*2;
   
   const centeredLines = lines.map(line => {
     const leftSpacing = " ".repeat((signWidth - line.length)/2);
     const rightSpacing = " ".repeat(signWidth - line.length - leftSpacing.length);
-    return `${leftSpacing}${line}${rightSpacing}`;
+    return `|${leftSpacing}${line}${rightSpacing}|`;
   });
-  output.value = centeredLines.join('\n');
+  const signHorizontalBar = '-'.repeat(signWidth+2);
+  output.value = `${signHorizontalBar}\n${centeredLines.join('\n')}\n${signHorizontalBar}`
 }
