26cd0251acba037f816d9bd2fbaeaf89bf44040e
jackding96 <jackding96@outlook.com>
2019-02-10 12:07:09 -0500

    fix spacing for the bunny

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const input = document.getElementById('input');
 const output = document.getElementById('output');
 
-input.addEventListener('keyup', inputHandler);
+input.addEventListener('keyup', bunnyBuilder);
 
-function inputHandler() {
-  const bunny = '(\\__/) ||\n(\u2022\u3145\u2022) ||\n/    \u3065';
+function bunnyBuilder() {
   
   const padding = 3;
   const lines = input.value.split('\n');
@@ -16,5 +15,10 @@ function inputHandler() {
     return `|${leftSpacing}${line}${rightSpacing}|`;
   });
   const signHorizontalBar = '-'.repeat(signWidth+2);
+
+  // Build the bunny
+  const bunnySpacing = signWidth/2 - 8 <= 0 ? "" : " ".repeat(signWidth/2 - 8);
+  const bunny = `${bunnySpacing}(\\__/) ||\n${bunnySpacing}(\u2022\u3145\u2022) ||\n${bunnySpacing}/    \u3065`;
+
   output.value = `${signHorizontalBar}\n${centeredLines.join('\n')}\n${signHorizontalBar}\n${bunny}`;
 }
