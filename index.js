c0e47c0731f1ee22a5f059b3742bda2473c2e90f
jackding96 <jackding96@outlook.com>
2019-02-10 11:29:16 -0500

    prog on special character encodings

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const input = document.getElementById('input');
 const output = document.getElementById('output');
 
+
+
 input.addEventListener('keyup', inputHandler);
 
 function inputHandler() {
-  console.log('(\\__/) ||\n(•ㅅ•) ||\n/ 　 づ');
-  
-//   `
-//   (\__/)    ||
-//   (• &#12613; •)   ||
-//  /  　  づ  
-//   `
+  console.log('\uffb5');
+  console.log('\u00B7');
 
-  const padding = 3;
-  const lines = input.value.split('\n');
-  const signWidth = Math.max(...lines.map(l=>l.length))+padding*2;
+//   console.log('(\\__/) ||\n(•ㅅ•) ||\n/ 　 づ');
   
-  const centeredLines = lines.map(line => {
-    const leftSpacing = " ".repeat((signWidth - line.length)/2);
-    const rightSpacing = " ".repeat(signWidth - line.length - leftSpacing.length);
-    return `|${leftSpacing}${line}${rightSpacing}|`;
-  });
-  const signHorizontalBar = '-'.repeat(signWidth+2);
-  output.value = `${signHorizontalBar}\n${centeredLines.join('\n')}\n${signHorizontalBar}\n${bunny}`;
+// //   `
+// //   (\__/)    ||
+// //   (• &#12613; •)   ||
+// //  /  　  づ  
+// //   `
+
+//   const padding = 3;
+//   const lines = input.value.split('\n');
+//   const signWidth = Math.max(...lines.map(l=>l.length))+padding*2;
+  
+//   const centeredLines = lines.map(line => {
+//     const leftSpacing = " ".repeat((signWidth - line.length)/2);
+//     const rightSpacing = " ".repeat(signWidth - line.length - leftSpacing.length);
+//     return `|${leftSpacing}${line}${rightSpacing}|`;
+//   });
+//   const signHorizontalBar = '-'.repeat(signWidth+2);
+//   output.value = `${signHorizontalBar}\n${centeredLines.join('\n')}\n${signHorizontalBar}\n${bunny}`;
 }
---
+++ server.js
@@ -0,0 +1,9 @@
+var signBunny = require('sign-bunny');  
+var asciiArt = signBunny('Best NPM Package Ever');
+console.log(asciiArt);
+
+console.log(`
+(\__/) ||
+(•ㅅ•) ||
+/ 　 づ
+`);
